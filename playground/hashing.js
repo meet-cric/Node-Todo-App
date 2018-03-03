2f7f45a7a5c25ec65c7f72aaa121b980c5d53d20
meet_cric <mp32445@gmai.com>
2018-03-04 00:13:21 +0530

    adding hash password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,28 +1,17 @@
 var SHA256 = require("crypto-js/sha256");
 var jwt=require('jsonwebtoken');
-var data={
-  id:5
-}
-var tokens=jwt.sign(data,"abc123");
-console.log(tokens);
-var result=jwt.verify(tokens,'abc123');
-console.log(result);
-// var message="my name is meet";
-// var hash=SHA256(message).toString();
-//
-// var data={
-//   id:14
-// }
-// var token={
-//   data,
-//   hash:JSON.stringify((data)+"something secret").toString()
-// }
-//
-// var resulthash=JSON.stringify((token.data)+"something secret").toString();
-//
-// if(resulthash===token.hash){
-//   console.log("keep fucking on");
-// }
-// else{
-//   console.log('u are fucked up');
-// }
+var bcrypt=require('bcryptjs');
+
+var password='meet123';
+
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash('password',salt,(err,hash)=>{
+    // console.log(hash);
+  })
+})
+var hashedpassword='$2a$10$AlAcOrmx20eA9AgelgDeTuySl/8ZWc0pXEaK8mayF31Q.QJYwtl32';
+
+bcrypt.compare('meet123',password,(err,res)=>{
+  console.log(res);
+})
+ 
--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID}=require('mongodb');
-const _=require('lodash')
+
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-const port= process.env.PORT||3000;
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
@@ -24,39 +27,49 @@ app.post('/todos', (req, res) => {
   });
 });
 
-
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-  })
-});
-app.get('/todos/:id',(req,res)=>{
-  var id=req.params.id;
-
-
-  Todo.findById(id).then((todo)=>{
-      if(!todo){
-        return res.status(404).send();
-      }
-      res.send({todo});
-  }).catch((e)=>{
-    return res.status(404).send();
   });
-})
-app.delete('/todos/:id',(req,res)=>{
-  var id=req.params.id;
-  Todo.findByIdAndRemove(id).then((res)=>{
-    if(!res){
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
       return res.status(404).send();
     }
-    res.send({res});
-  }).catch((e)=>{
-      return res.status(404).send();
-  })
-})
 
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.patch('/todos/:id', (req, res) => {
   var id = req.params.id;
@@ -83,23 +96,27 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   })
 });
-//post/user/todo
-app.post('/users',(req,res)=>{
-  var body=_.pick(req.body,['email','password']);
-  var user=new User(body);
 
-  user.save().then((user)=>{
-   return user.generateAuthToken();
- }).then((token)=>{
-   res.header('x-auth',token).send(user)
-  }).catch((e)=>{
-    res.status(404).send(e);
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
   })
 });
 
-
-app.listen(port, () => {
-  console.log('Started on port 3000');
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
-module.export={app};
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+module.exports = {app};
